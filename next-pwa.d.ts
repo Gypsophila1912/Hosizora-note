380e6a0cb98fd9cb85b69f2f25991d58a343adf6
Gypsophila1912 <noma.19.anko@gmail.com>
2026-02-07 18:56:07 +0900

    thirdコミット

---
+++ next-pwa.d.ts
@@ -0,0 +1,18 @@
+// next-pwa.d.ts
+declare module "next-pwa" {
+  import { NextConfig } from "next";
+
+  interface PWAConfig {
+    dest: string;
+    register?: boolean;
+    skipWaiting?: boolean;
+    disable?: boolean;
+    sw?: string;
+    publicExcludes?: string[];
+    buildExcludes?: (string | RegExp)[];
+  }
+
+  function withPWA(config: PWAConfig): (nextConfig: NextConfig) => NextConfig;
+
+  export default withPWA;
+}
