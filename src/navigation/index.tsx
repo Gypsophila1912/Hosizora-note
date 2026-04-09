c7bb3b1920269c36e8addad7dbdf3982e9b083de
Gypsophila1912 <noma.19.anko@gmail.com>
2026-04-09 17:36:23 +0900

    chore フォルダ構成修正

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,4 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
+//↑画面遷移を管理するためのコンポーネント。ブラウザの役割を持つ。画面移動、戻るボタンの管理、ナビゲーション状態の管理等
 import AppRoutes from "./routes/AppRoutes";
 
 export default function Navigation() {
